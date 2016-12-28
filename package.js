c9374992df7be176e8e37a5e17d4c1bfc300f7c1
VansonLeung <vanson-hk@hotmail.com>
2016-12-28 09:20:33 +0800

    Update package.js

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
 	name: 'vansonhk:autoform-ace',
 	summary: 'Ace editor for autoform.',
 	git: 'https://github.com/VansonLeung/autoform-ace.git',
-	version: '0.0.4'
+	version: '0.0.5'
 });
 
 Package.onUse(function(api) {
