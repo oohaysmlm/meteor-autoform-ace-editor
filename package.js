ef9f1da10ad5e2615457f4e77b3c9082547c0dc0
Daniel Metzger <daniel@bittiger.io>
2017-10-16 13:24:18 -0700

    Update version

--- package.js
+++ package.js
@@ -3,7 +3,7 @@ Package.describe({
 	name: 'skehoe:autoform-ace-multilang',
 	summary: 'Ace editor for autoform.',
 	git: 'https://github.com/VansonLeung/autoform-ace.git',
-	version: '0.0.13'
+	version: '0.0.14'
 });
 
 Package.onUse(function(api) {
